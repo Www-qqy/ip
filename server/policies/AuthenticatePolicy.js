1e74d5b26b0669a43273fef1b74c7ccc61d0b588
wwwqqy <1793472018@qq.com>
2020-05-01 17:43:57 +0800

    懵逼*6

---
+++ server/policies/AuthenticatePolicy.js
@@ -0,0 +1,31 @@
+const Jwt = require('jsonwebtoken')
+const config = require('../config')
+function tokenVerify(token) {
+  try {
+    Jwt.verify(token, config.token.secretOrPrivateKey)
+    return true
+  } catch (error) {
+    return false
+  }
+}
+module.exports = {
+  isValidToken(req, res, next) {
+    let bearerToken = req.headers.authorization
+    try {
+      let token = bearerToken.split(' ')[1]
+      if (tokenVerify(token)) {
+        next()
+      } else {
+        res.status(403).send({
+          code: 403,
+          error: '登陆无效请重新登录',
+        })
+      }
+    } catch (error) {
+      res.status(401).send({
+        code: 401,
+        error: '请登陆后再访问',
+      })
+    }
+  },
+}
--- server/router.js
+++ server/router.js
@@ -1,11 +1,24 @@
 const UserController = require('./controllers/UserController')
+const AuthenticatePolicy = require('./policies/AuthenticatePolicy')
 module.exports = (app) => {
-  app.get('/api', (req, res) => {
-    res.send({
-      msg: 'hello node router',
-    })
-  })
-  app.get('/users/:id', UserController.getUserById)
+  // app.post('/user/login',UserController.login)
+  // app.get('/api', (req, res) => {
+  //   res.send({
+  //     msg: 'hello node router',
+  //   })
+  // })
+  // app.get(
+  //   '/users/:id',
+  //   AuthenticatePolicy.isValidToken,
+  //   AuthenticatePolicy.getUserById
+  // )
+
+  app.get(
+    '/users/:id',
+    AuthenticatePolicy.isValidToken,
+    UserController.getUserById
+  )
+
   app.post('/users', UserController.register)
   app.put('/users/:id', UserController.update)
   app.delete('/users/:id', UserController.delete)
