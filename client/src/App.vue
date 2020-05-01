1512d6fb27da9eae1b14d2ed49f8ee57facab6a5
wwwqqy <1793472018@qq.com>
2020-05-02 01:20:21 +0800

    攻击线地图

--- client/src/App.vue
+++ client/src/App.vue
@@ -15,6 +15,7 @@ export default {
 * {
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -22,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
